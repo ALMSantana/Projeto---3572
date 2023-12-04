7e6586fc9cf3b31f39740847bfde7b6b47dd5388
ALMSantana <andrelms4@insper.edu.br>
2023-12-04 17:36:13 -0300

    Aula 4.1

---
+++ analisador_sentimentos.py
@@ -0,0 +1,63 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+def carrega(nome_do_arquivo):
+    try:
+        with open(nome_do_arquivo, "r") as arquivo:
+            dados = arquivo.read()
+            return dados
+    except IOError as e:
+        print(f"Erro: {e}")
+
+def salva(nome_do_arquivo, conteudo):
+    try:
+        with open(nome_do_arquivo, "w", encoding="utf-8") as arquivo:
+            arquivo.write(conteudo)
+    except IOError as e:
+        print(f"Erro ao salvar arquivo: {e}")
+
+def analisador_sentimentos(produto):
+    prompt_sistema = f"""
+        Você é um analisador de sentimentos de avaliações de produtos.
+        Escreva um parágrafo com até 50 palavras resumindo as avaliações e 
+        depois atribua qual o sentimento geral para o produto.
+        Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+
+        # Formato de Saída
+
+        Nome do Produto:
+        Resumo das Avaliações:
+        Sentimento Geral: [utilize aqui apenas Positivo, Negativo ou Neutro]
+        Ponto fortes: lista com três bullets
+        Pontos fracos: lista com três bullets
+    """
+     
+    prompt_usuario = carrega(f"./dados/avaliacoes-{produto}.txt")
+    print(f"Inicou a análise de sentimentos do produto {produto}")
+
+    lista_mensagens = [
+        {
+            "role": "system",
+            "content": prompt_sistema
+        },
+        {
+            "role": "user",
+            "content": prompt_usuario
+        }
+    ]
+
+    resposta = cliente.chat.completions.create(
+            messages = lista_mensagens,
+            model=modelo
+        )
+
+    texto_resposta = resposta.choices[0].message.content
+    salva(f"./dados/analise-{produto}.txt", texto_resposta)
+
+analisador_sentimentos("Maquiagem mineral")
